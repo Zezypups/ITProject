42180c3c13a4e4307fdc98ce8a13555105fc3990
Zezypups <109433579+Zezypups@users.noreply.github.com>
2023-03-28 22:27:54 +0400

    Added responsive cards

--- static/js/shop_popup.js
+++ static/js/shop_popup.js
@@ -1,14 +1,14 @@
 let popup = document.getElementById('mypopup'),
-        popupToggle = document.getElementById('exchange'),
-        popupClose = document.querySelector('.close');
+                popupToggle = document.getElementById('exchange'),
+                popupClose = document.querySelector('.close');
 
-        popupToggle.onclick = function() {
-        popup.style.display="block";
-        };
+                popupToggle.onclick = function() {
+                    popup.style.display="block";
+                };
 
-        popupClose.onclick = function () {
-        popup.style.display="none";
-        }
+                popupClose.onclick = function () {
+                    popup.style.display="none";
+                }
         //Сделать так чтобы при нажатии в любое место на экране или при
         // нажатии на esc закрывалось модальное окно 
 
